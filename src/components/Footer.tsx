132df98bc2dee4bf8234f573e084aca3a066149f
Oscar Zealley <oscar@zealley.com>
2020-11-18 14:48:16 +0000

    [OZ] add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,56 @@
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styled from 'styled-components';
+import { P } from './Typography';
+
+const StyledFooter = styled.footer`
+  padding-bottom: ${({ theme }) => theme.spacer['xl']};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  &:hover {
+    color: ${({ theme }) => theme.colour.pink};
+    transition: ${({ theme }) => theme.transition.default};
+  }
+`;
+
+const StyledLink = styled.a`
+  margin-left: ${({ theme }) => theme.spacer['2']};
+  margin-right: ${({ theme }) => theme.spacer['2']};
+  text-decoration: none;
+  color: inherit;
+`;
+
+const IconContainer = styled.div`
+  padding-bottom: ${({ theme }) => theme.spacer[8]};
+  display: flex;
+  justify-content: center;
+`;
+
+const Footer: React.FC = () => {
+  return (
+    <StyledFooter>
+      <IconContainer>
+        <StyledLink href="https://github.com/oscar8880" target="_blank">
+          <StyledIcon size="3x" color="#020202" icon={faGithubSquare} />
+        </StyledLink>
+        <StyledLink
+          href="https://www.linkedin.com/in/oscar-zealley-81a027123/"
+          target="_blank"
+        >
+          <StyledIcon size="3x" color="#020202" icon={faLinkedin} />
+        </StyledLink>
+      </IconContainer>
+      <P>
+        © {new Date().getFullYear()} Oscar Zealley, Built with{' '}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </P>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,40 +1,19 @@
 import React from 'react';
-// import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './Header';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { defaultTheme, GlobalStyle } from '../theme/globalStyles';
 import PageWrapper from './PageWrapper';
-
-const Footer = styled.footer`
-  margin-top: 2rem;
-`;
+import Footer from './Footer';
 
 const Layout: React.FC = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `);
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <PageWrapper>
         <Header />
         <main>{children}</main>
-        <Footer
-          style={{
-            marginTop: '2rem',
-          }}
-        >
-          © {new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </Footer>
+        <Footer />
       </PageWrapper>
     </ThemeProvider>
   );
