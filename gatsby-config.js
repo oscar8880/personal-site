913231ef07de284ed4cee88a47694ed5b32dc017
Oscar Zealley <oscar@zealley.com>
2020-11-22 18:44:42 +0000

    [OZ] add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Oscar Zealley',
+    description: "Oscar Zealley's portfolio website",
+    author: 'Oscar Zealley',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -31,13 +30,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Oscar Zealley',
+        short_name: 'Oscar',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#f5f5f5',
+        theme_color: '#020202',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png',
+        icon: 'src/images/oLogo.svg',
       },
     },
     'gatsby-plugin-offline',
